9bf58031b363038cc70281eeef6e71d22938481c
theblueprint415 <justin.chizer@gmail.com>
2016-07-15 15:21:32 -0700

    3:21 PST

--- WindowsApp/YelpPage.xaml.cs
+++ WindowsApp/YelpPage.xaml.cs
@@ -47,6 +47,7 @@ namespace WindowsApp
         {
             // Create a client
             HttpClient httpClient = new HttpClient();
+            
 
             string ZOMATO_API = "https://developers.zomato.com/api/v2.1/search?q=";
 
@@ -69,19 +70,56 @@ namespace WindowsApp
             string responseAsString = await response.Content.ReadAsStringAsync();
             //OutputField.Text = responseAsString;
 
-            RootObject rootObject = JsonConvert.DeserializeObject<RootObject>(responseAsString);
+            RootObject rootObject = null;
+            
+            rootObject = JsonConvert.DeserializeObject<RootObject>(responseAsString);
 
-
-
-            //name0.Text = "Name: " + rootObject.restaurants[0].restaurant.name;
-            //address0.Text = rootObject.restaurants[0].restaurant.location.address;
+            //Info for Restaurant 0
+            name0.Text = rootObject.restaurants[0].restaurant.name;
+            address0.Text = rootObject.restaurants[0].restaurant.location.address;
+            average_cost_for_two0.Text = "Cost for two: $" + rootObject.restaurants[0].restaurant.average_cost_for_two.ToString();
             image0.Source = new BitmapImage(new Uri(rootObject.restaurants[0].restaurant.thumb, UriKind.Absolute));
+
+            //Info for Restaurant 1
+            name1.Text = rootObject.restaurants[1].restaurant.name;
+            address1.Text = rootObject.restaurants[1].restaurant.location.address;
+            average_cost_for_two1.Text = "Cost for two: $" + rootObject.restaurants[1].restaurant.average_cost_for_two.ToString();
             image1.Source = new BitmapImage(new Uri(rootObject.restaurants[1].restaurant.thumb, UriKind.Absolute));
+
+            //Info for Restaurant 2
+            name2.Text = rootObject.restaurants[2].restaurant.name;
+            address2.Text = rootObject.restaurants[2].restaurant.location.address;
+            average_cost_for_two2.Text = "Cost for two: $" + rootObject.restaurants[2].restaurant.average_cost_for_two.ToString();
             image2.Source = new BitmapImage(new Uri(rootObject.restaurants[2].restaurant.thumb, UriKind.Absolute));
+
+            //Info for Restaurant 3
+            name3.Text = rootObject.restaurants[3].restaurant.name;
+            address3.Text = rootObject.restaurants[3].restaurant.location.address;
+            average_cost_for_two3.Text = "Cost for two: $" + rootObject.restaurants[3].restaurant.average_cost_for_two.ToString();
             image3.Source = new BitmapImage(new Uri(rootObject.restaurants[3].restaurant.thumb, UriKind.Absolute));
+
+            //Info for Restaurant 4
+            name4.Text = rootObject.restaurants[4].restaurant.name;
+            address4.Text = rootObject.restaurants[4].restaurant.location.address;
+            average_cost_for_two4.Text = "Cost for two: $" + rootObject.restaurants[4].restaurant.average_cost_for_two.ToString();
             image4.Source = new BitmapImage(new Uri(rootObject.restaurants[4].restaurant.thumb, UriKind.Absolute));
+
+            //Info for Restaurant 5
+            name5.Text = rootObject.restaurants[5].restaurant.name;
+            address5.Text = rootObject.restaurants[5].restaurant.location.address;
+            average_cost_for_two5.Text = "Cost for two: $" + rootObject.restaurants[5].restaurant.average_cost_for_two.ToString();
             image5.Source = new BitmapImage(new Uri(rootObject.restaurants[5].restaurant.thumb, UriKind.Absolute));
+
+            //Info for Restaurant 6
+            name6.Text = rootObject.restaurants[6].restaurant.name;
+            address6.Text = rootObject.restaurants[6].restaurant.location.address;
+            average_cost_for_two6.Text = "Cost for two: $" + rootObject.restaurants[6].restaurant.average_cost_for_two.ToString();
             image6.Source = new BitmapImage(new Uri(rootObject.restaurants[6].restaurant.thumb, UriKind.Absolute));
+
+            //Info for Restaurant 7
+            name7.Text = rootObject.restaurants[7].restaurant.name;
+            address7.Text = rootObject.restaurants[7].restaurant.location.address;
+            average_cost_for_two7.Text = "Cost for two: $" + rootObject.restaurants[7].restaurant.average_cost_for_two.ToString();
             image7.Source = new BitmapImage(new Uri(rootObject.restaurants[7].restaurant.thumb, UriKind.Absolute));
 
         }
